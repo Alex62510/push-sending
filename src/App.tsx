a1b26bf0b9d48b542cfd3bda2ff9efbc6cc65cf6
Alex Orlov <eagle62510@gmail.com>
2026-07-02 14:28:13 +0300

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+
+      <main className="page">
+        <h1>Push рассылка</h1>
+        <p>Страница для подключения и проверки уведомлений</p>
+      </main>
+    </>
+  )
+}
+
+export default App
